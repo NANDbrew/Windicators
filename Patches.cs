7ff5345ee6a37902388d4997567ffb9b988c5269
NANDbrew <zortman@gmail.com>
2025-07-31 23:39:17 -0500

    adjust logging

--- Patches.cs
+++ Patches.cs
@@ -13,7 +13,7 @@ namespace Windicators
         internal static void Prefix()
         {
             if (AssetTools.bundle == null) AssetTools.LoadAssetBundles();
-            //if (PrefabsDirectory.instance.directory.Length < 522) Array.Resize(ref PrefabsDirectory.instance.directory, 522);
+
             foreach (var item in AssetTools.itemPrefabs)
             {
                 if (item.Key >= PrefabsDirectory.instance.directory.Length)
@@ -39,14 +39,21 @@ namespace Windicators
         internal static void Postfix(IslandStreetlightsManager __instance)
         {
             if (AssetTools.bundle == null) AssetTools.LoadAssetBundles();
-            Debug.Log("horizonPatches: patching shopkeeper");
             int index = __instance.gameObject.GetComponent<IslandSceneryScene>().parentIslandIndex;
+#if DEBUG
+            Debug.Log("Windicators: patching shopkeeper on island " + index);
+#endif
             if (AssetTools.shopKeepers.ContainsKey(index))
             {
                 var shopkeeper = UnityEngine.Object.Instantiate(AssetTools.shopKeepers[index], __instance.transform);
                 shopkeeper.shopPrefab.transform.parent = __instance.transform;
-#if DEBUG
+
                 Debug.Log($"Windicators: Adding shopkeeper for island {index}");
+            }
+            else
+            {
+#if DEBUG
+                Debug.LogWarning($"Windicators: No shopkeeper found for island {index}");
 #endif
             }
         }
@@ -60,37 +67,35 @@ namespace Windicators
         [HarmonyPostfix]
         public static void EnterPatch(ShipItem __instance)
         {
-            Debug.Log("into the pocket");
-            //__instance.GetComponent<InvSwitcherWindCompass>()?.OnEnterInventory();
-            __instance.GetComponent<InvSwitcher>()?.OnEnterInventory();//ToggleFlag(false);
+            //Debug.Log("into the pocket");
+            __instance.GetComponent<InvSwitcher>()?.OnEnterInventory();
         }
         [HarmonyPatch("OnLeaveInventory")]
         [HarmonyPostfix]
         internal static void ExitPatch(ShipItem __instance)
         {
-            Debug.Log("out of the pocket");
-            //__instance.GetComponent<InvSwitcherWindCompass>()?.OnLeaveInventory();
-            __instance.GetComponent<InvSwitcher>()?.OnLeaveInventory();// ToggleFlag(true);
+            //Debug.Log("out of the pocket");
+            __instance.GetComponent<InvSwitcher>()?.OnLeaveInventory();
         }
         [HarmonyPatch("OnDrop")]
         [HarmonyPostfix]
         public static void DropPatch(ShipItemHangable __instance)
         {
-            Debug.Log("dropped");
+            //Debug.Log("dropped");
             __instance.GetComponent<InvSwitcher>()?.OnDrop();
         }
         [HarmonyPatch("OnPickup")]
         [HarmonyPostfix]
         internal static void PickupPatch(ShipItem __instance)
         {
-            Debug.Log("picked up");
+            //Debug.Log("picked up");
             __instance.GetComponent<InvSwitcher>()?.OnPickup();
         }
         [HarmonyPatch("ReturnToShopPos")]
         [HarmonyPostfix]
         internal static void ReturnToShopPatch(ShipItem __instance)
         {
-            Debug.Log("drop");
+            //Debug.Log("drop");
             __instance.GetComponent<InvSwitcher>()?.OnDrop();
         }
     }
